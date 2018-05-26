cddf81c76deb89d79011469f68ab7c87a4d85f06
Josh <kaio11602@gmail.com>
2018-05-25 19:00:04 -0700

    Update Slideshow.plugin.js

--- Slideshow.plugin.js
+++ Slideshow.plugin.js
@@ -2,14 +2,16 @@
 
 // Delay (in seconds) between image changes
 var delay = 900;
+// Transition speed (in milliseconds)
+var speed = 1500;
 // List of all pictures to use as backgrounds
-var links = ["https://i.imgur.com/Ey80Lpp.jpg", "https://i.imgur.com/t0v923r.jpg", "https://i.imgur.com/JTtvGOw.jpg"];
+var links = ["https://i.imgur.com/Ey80Lpp.jpg", "https://i.imgur.com/t0v923r.jpg", "https://i.imgur.com/JTtvGOw.jpg", "https://i.imgur.com/avzaqfC.jpg", "https://i.imgur.com/MPXZh2E.jpg"];
 // Opacity of each picture to use as a background
-var opacity = [0.55, 0.65, 0.7];
+var opacity = [0.55, 0.65, 0.7, 0.65, 0.3];
 // Color of transparent screen over background
-var screenColor = ["0, 0, 0", "0, 0, 0", "0, 0, 0"];
+var screenColor = ["0, 0, 0", "0, 0, 0", "0, 0, 0", "0, 0, 0", "0, 0, 0"];
 
-
+var hasRun = false;
 var interval = 0;
 
 class Slideshow {
@@ -18,35 +20,33 @@ class Slideshow {
     getVersion() { return "1.0"; }
     getAuthor() { return "PseudoResonance"; }
 
-    load() {
-		$('<div id="betterdiscord-background" style="width:100%;height:100%;left:0px;top:0px;position:absolute;"></div>').insertBefore('.appMount-3VJmYg');
-		for (var i = 0; i < links.length; i++) {
-            $('#betterdiscord-background').append('<div style="display:none;width:100%;height:100%;left:0px;top:0px;position:absolute;background-size:cover;z-index:-3;background-image:linear-gradient(to right,rgba(' + screenColor[i] + ', ' + opacity[i] + ') 0%,rgba(' + screenColor[i] + ', ' + opacity[i] + ') 100%),url(' + links[i] + ');"></div>');
-		}
-	}
+    load() {}
 
     start() {
+		if (!hasRun) {
+			hasRun = true;
+			$('<div id="betterdiscord-background" style="width:100%;height:100%;left:0px;top:0px;position:absolute;"></div>').insertBefore('.appMount-3VJmYg');
+			for (var i = 0; i < links.length; i++) {
+				$('#betterdiscord-background').append('<div style="display:none;width:100%;height:100%;left:0px;top:0px;position:absolute;background-size:cover;z-index:-3;background-image:linear-gradient(to right,rgba(' + screenColor[i] + ', ' + opacity[i] + ') 0%,rgba(' + screenColor[i] + ', ' + opacity[i] + ') 100%),url(' + links[i] + ');"></div>');
+			}
+		}
         $('#betterdiscord-background div:nth-child(1)').show();
 		var i = 0;
         interval = window.setInterval(function(){
             i++;
             if (i >= links.length) {
-				$('#betterdiscord-background div:nth-child(1)').css("z-index", "-1").fadeIn(1000);
+				$('#betterdiscord-background div:nth-child(1)').css("z-index", "-1").fadeIn(speed);
 				setTimeout(function() {
 					$('#betterdiscord-background div:nth-child(' + i + ')').hide();
 					i = 0;
-				}, 1500);
-				setTimeout(function() {
 					$('#betterdiscord-background div:nth-child(1)').css("z-index", "-3");
-				}, 1500);
+				}, speed + 1);
             } else {
-				$('#betterdiscord-background div:nth-child(' + (i + 1) + ')').css("z-index", "-1").fadeIn(1000);
+				$('#betterdiscord-background div:nth-child(' + (i + 1) + ')').css("z-index", "-1").fadeIn(speed);
 				setTimeout(function() {
 					$('#betterdiscord-background div:nth-child(' + i + ')').hide();
-				}, 1500);
-				setTimeout(function() {
 					$('#betterdiscord-background div:nth-child(' + (i + 1) + ')').css("z-index", "-3");
-				}, 1500);
+				}, speed + 1);
 			}
         }, delay * 1000);
     }
