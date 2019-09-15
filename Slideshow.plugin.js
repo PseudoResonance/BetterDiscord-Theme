1156a0ebc583606eaa46b7f71b48f797a959c1f6
Josh <kaio11602@gmail.com>
2019-09-14 18:42:25 -0700

    Update Slideshow.plugin.js

--- Slideshow.plugin.js
+++ Slideshow.plugin.js
@@ -1,40 +1,47 @@
 //META{"name":"Slideshow"}*//
 
 // Delay (in seconds) between image changes
+// Default: 600
 var delay = 600;
 // Transition speed (in milliseconds)
+// Default: 1500
 var speed = 1500;
-// List of all pictures to use as backgrounds
-var links = ["https://i.imgur.com/Ey80Lpp.jpg", "https://i.imgur.com/t0v923r.jpg", "https://i.imgur.com/JTtvGOw.jpg", "https://i.imgur.com/avzaqfC.jpg", "https://i.imgur.com/MPXZh2E.jpg", "https://i.imgur.com/x6oKs2B.jpg", "https://i.imgur.com/0xTS9zY.jpg", "https://i.imgur.com/fEZUUxQ.jpg", "https://i.imgur.com/OBIAgWl.jpg"];
-// Opacity of each picture to use as a background
-var opacity = [0.6, 0.5, 0.6, 0.5, 0.3, 0.50, 0.4, 0.45, 0.5];
-// Color of transparent screen over background
-var screenColor = ["0, 0, 0", "0, 0, 0", "0, 0, 0", "0, 0, 0", "0, 0, 0", "0, 0, 0", "0, 0, 0", "0, 0, 0", "0, 0, 0"];
+// List of all pictures to use as backgrounds with opacity and color of transparent screen
+var backgrounds = [
+{link:"https://i.imgur.com/xjmfk81.jpg", opacity:0.5, color:"0, 0, 0"},
+{link:"https://i.imgur.com/XDzFDVJ.jpg", opacity:0.55, color:"0, 0, 0"},
+{link:"https://i.imgur.com/Ey80Lpp.jpg", opacity:0.65, color:"0, 0, 0"},
+{link:"https://i.imgur.com/t0v923r.jpg", opacity:0.5, color:"0, 0, 0"},
+{link:"https://i.imgur.com/JTtvGOw.jpg", opacity:0.6, color:"0, 0, 0"},
+{link:"https://i.imgur.com/avzaqfC.jpg", opacity:0.55, color:"0, 0, 0"},
+{link:"https://i.imgur.com/MPXZh2E.jpg", opacity:0.4, color:"0, 0, 0"},
+{link:"https://i.imgur.com/x6oKs2B.jpg", opacity:0.55, color:"0, 0, 0"},
+{link:"https://i.imgur.com/0xTS9zY.jpg", opacity:0.45, color:"0, 0, 0"},
+{link:"https://i.imgur.com/fEZUUxQ.jpg", opacity:0.5, color:"0, 0, 0"},
+{link:"https://i.imgur.com/OBIAgWl.jpg", opacity:0.55, color:"0, 0, 0"}
+];
 
-var hasRun = false;
 var interval = 0;
 
 class Slideshow {
     getName() { return "Slideshow"; }
     getDescription() { return "Turns the background into a slideshow"; }
-    getVersion() { return "1.0"; }
+    getVersion() { return "2.0"; }
     getAuthor() { return "PseudoResonance"; }
 
     load() {}
 
     start() {
-		if (!hasRun) {
-			hasRun = true;
-			$('<div id="betterdiscord-background" style="width:100%;height:100%;left:0px;top:0px;position:absolute;"></div>').insertBefore('#app-mount');
-			for (var i = 0; i < links.length; i++) {
-				$('#betterdiscord-background').append('<div style="display:none;width:100%;height:100%;left:0px;top:0px;position:absolute;background-size:cover;z-index:-3;background-image:linear-gradient(to right,rgba(' + screenColor[i] + ', ' + opacity[i] + ') 0%,rgba(' + screenColor[i] + ', ' + opacity[i] + ') 100%),url(' + links[i] + ');"></div>');
-			}
+		$('#betterdiscord-background').remove();
+		$('<div id="betterdiscord-background" style="width:100%;height:100%;left:0px;top:0px;position:absolute;"></div>').insertBefore('#app-mount');
+		for (var i = 0; i < backgrounds.length; i++) {
+			$('#betterdiscord-background').append('<div style="display:none;width:100%;height:100%;left:0px;top:0px;position:absolute;background-size:cover;z-index:-3;background-image:linear-gradient(to right,rgba(' + backgrounds[i].color + ', ' + backgrounds[i].opacity + ') 0%,rgba(' + backgrounds[i].color + ', ' + backgrounds[i].opacity + ') 100%),url(' + backgrounds[i].link + ');"></div>');
 		}
         $('#betterdiscord-background div:nth-child(1)').show();
 		var i = 0;
         interval = window.setInterval(function(){
             i++;
-            if (i >= links.length) {
+            if (i >= backgrounds.length) {
 				$('#betterdiscord-background div:nth-child(1)').css("z-index", "-1").fadeIn(speed);
 				setTimeout(function() {
 					$('#betterdiscord-background div:nth-child(' + i + ')').hide();
