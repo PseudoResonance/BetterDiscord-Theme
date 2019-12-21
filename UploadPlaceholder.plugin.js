f236f809df07db02f18ac13d954753345ecf2528
Josh <kaio11602@gmail.com>
2019-12-20 19:33:02 -0800

    More resilient to updates

--- UploadPlaceholder.plugin.js
+++ UploadPlaceholder.plugin.js
@@ -5,7 +5,7 @@ var interval = 0;
 class UploadPlaceholder {
     getName() { return "Upload Placeholder"; }
     getDescription() { return "Adds a placeholder to the upload box"; }
-    getVersion() { return "1.1"; }
+    getVersion() { return "1.2"; }
     getAuthor() { return "PseudoResonance"; }
 
     load() {}
@@ -18,11 +18,16 @@ class UploadPlaceholder {
 			});
 			text = text.substring(0, text.length - 1).replace(/\n/g, " ");
 			if (text.length > 0) {
-				var input = $('.da-uploadModal .da-inner .da-comment .da-channelTextArea .da-scrollableContainer .da-inner .da-textArea .da-slateTextArea');
+				var input = $('.da-uploadModal .da-inner .da-comment .da-channelTextArea .scrollableContainer-2NUZem .da-inner .da-textArea .da-slateTextArea');
+				var samplePlaceholder = $('.da-form .da-channelTextArea .scrollableContainer-2NUZem .da-inner .da-textArea .da-placeholder');
 				if (input.text().trim() != "") text = ""
 				input.attr('placeholder', text);
+				var classes = "placeholder-37qJjk da-placeholder";
+				if (samplePlaceholder.length !== 0)
+					if (samplePlaceholder.attr('class').length > 0)
+						classes = samplePlaceholder.attr('class');
 				if ($('#pseudo-customUploadModalPlaceholder').length === 0) {
-					input.before($("<div class='placeholder-P6ptfj da-placeholder' id='pseudo-customUploadModalPlaceholder'>" + text + "</div>"));
+					input.before($("<div class='" + classes + "' id='pseudo-customUploadModalPlaceholder'>" + text + "</div>"));
 				} else {
 					if ($('#pseudo-customUploadModalPlaceholder').html() != text)
 						$('#pseudo-customUploadModalPlaceholder').html(text);
