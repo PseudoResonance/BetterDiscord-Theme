0ef442c82d38f52b652d1494b551b37a364c137d
Josh <kaio11602@gmail.com>
2021-11-30 23:38:36 -0800

    Update URLDecode 1.0.2
    Fixed decoding truncated titles/descriptions.

--- URLDecode.plugin.js
+++ URLDecode.plugin.js
@@ -16,24 +16,23 @@ module.exports = (() => {
 					github_username: "PseudoResonance"
 				}
 			],
-			version: "1.0.1",
+			version: "1.0.2",
 			description: "Automatic URL decoder.",
 			github: "https://github.com/PseudoResonance/BetterDiscord-Theme/blob/master/URLDecode.plugin.js",
 			github_raw: "https://raw.githubusercontent.com/PseudoResonance/BetterDiscord-Theme/master/URLDecode.plugin.js"
 		},
-		changelog: [
-			{
+		changelog: [{
 				title: "Initial Release",
 				type: "added",
 				items: [
 					"Automatically decodes URLs in chat and embeds."
 				]
-			},
-			{
+			}, {
 				title: "Fixed",
 				type: "fixed",
 				items: [
-					"Fixed missing null check."
+					"Fixed missing null check.",
+					"Fixed decoding truncated titles/descriptions."
 				]
 			}
 		],
@@ -94,6 +93,218 @@ module.exports = (() => {
 				Patcher
 			} = Api;
 
+			class StripInvalidTrailingEncoding {
+				/**
+				 * https://github.com/jridgewell/strip-invalid-trailing-encoding
+				 *
+				 * MIT License
+				 *
+				 * Copyright (c) 2017 Justin Ridgewell
+				 *
+				 * Permission is hereby granted, free of charge, to any person obtaining a copy
+				 * of this software and associated documentation files (the "Software"), to deal
+				 * in the Software without restriction, including without limitation the rights
+				 * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+				 * copies of the Software, and to permit persons to whom the Software is
+				 * furnished to do so, subject to the following conditions:
+				 *
+				 * The above copyright notice and this permission notice shall be included in all
+				 * copies or substantial portions of the Software.
+				 *
+				 * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+				 * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+				 * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+				 * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+				 * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+				 * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+				 * SOFTWARE.
+				*/
+
+				/**
+				 * Parses a (possibly) hex char into its int value.
+				 * If the char is not valid hex char, returns 16.
+				 *
+				 * @param {string} char
+				 * @return {number}
+				 */
+				static toHex(char) {
+					const i = char.charCodeAt(0);
+					// 0 - 9
+					if (i >= 48 && i <= 57) {
+						return i - 48;
+					}
+
+					const a = (i | 0x20);
+					// Normalize A-F into a-f.
+					if (a >= 97 && a <= 102) {
+						return a - 87;
+					}
+
+					// Invalid Hex
+					return 16;
+				}
+
+				/**
+				 * Determines if a '%' character occurs in the last 3 characters of a string.
+				 * If none, returns 3.
+				 *
+				 * @param {string} string
+				 * @param {number} length
+				 * @return {boolean}
+				 */
+				static hasPercent(string, length) {
+					if (length > 0 && string[length - 1] === '%') {
+						return 1;
+					}
+					if (length > 1 && string[length - 2] === '%') {
+						return 2;
+					}
+					if (length > 2 && string[length - 3] === '%') {
+						return 0;
+					}
+
+					return 3;
+				}
+
+				/**
+				 * Strips invalid Percent Encodings that occur at the end of a string.
+				 * This is highly optimized to trim only _broken_ sequences at the end.
+				 *
+				 * Note that this **IS NOT** a string sanitizer. It will not prevent native
+				 * decodeURIComponent from throwing errors. This is only to prevent "good"
+				 * strings that were invalidly truncated in the middle of a percent encoding
+				 * from throwing. Attackers can craft strings will not be "fixed" by stripping.
+				 *
+				 * @param {string} string
+				 * @param {number} length The length of the string.
+				 * @param {number} shift Position of the rightmost %.
+				 * @return {string}
+				 */
+				static _strip(string, length, shift) {
+					let end = length - shift;
+					let num = -shift;
+					let high = '8';
+					let low = '0';
+					let continuation = false;
+
+					for (let pos = length - 1; pos >= 0; pos--) {
+						const char = string[pos];
+						num++;
+
+						if (char !== '%') {
+							// If we have backtracked 3 characters and we don't find a "%", we know the
+							// string did not end in an encoding.
+							if (num % 3 === 0) {
+								if (continuation) {
+									// Someone put extra continuations.
+									return '';
+								}
+
+								break;
+							}
+
+							// Else, we need to keep backtracking.
+							low = high;
+							high = char;
+							continue;
+						}
+
+						const h = this.toHex(high);
+						const l = this.toHex(low);
+						if (h === 16 || l === 16) {
+							// Someone put non hex values.
+							return '';
+						}
+
+						// &    %26
+						// %26  00100110
+						// α    %CE%B1
+						// %CE  11001110
+						// %B1  10110001
+						// ⚡   %E2%9A%A1
+						// %E2  11100010
+						// %9A  10011010
+						// %A1  10100001
+						// 𝝰    %F0%9D%9D%B0
+						// %F0  11110000
+						// %9D  10011101
+						// %9D  10011101
+						// %B0  10110000
+						// Single encodings are guaranteed to have a leading "0" bit in the byte.
+						// The first of a multi sequence always starts with "11" bits, while the
+						// "continuation"s always start with "10" bits.
+						// Spec: http://www.ecma-international.org/ecma-262/6.0/#table-43
+						const isSingle = (h & 8) === 0;
+						const isContinuationStart = (~h & 12) === 0;
+
+						if (isSingle || isContinuationStart) {
+							continuation = false;
+
+							// If a single is full (has 3 chars), we don't need to truncate it.
+							// If a continuation is full (chars depends on the offset of the leftmost
+							// "0" bit), we don't need to truncate it.
+							let escapes = 3;
+							if (isContinuationStart) {
+								if ((h & 2) === 0) {
+									escapes = 6;
+								} else if ((h & 1) === 0) {
+									escapes = 9;
+								} else if ((l & 8) === 0) {
+									escapes = 12;
+								} else if (num > 0 && num % 3 === 0) {
+									// Someone put random hex values together.
+									return '';
+								}
+							}
+
+							if (num > escapes) {
+								// Someone put extra continuations.
+								return '';
+							}
+
+							if (num < escapes) {
+								// We're at a broken sequence, truncate to here.
+								end = pos;
+							}
+
+							break;
+						} else {
+							// A trailing % does not count as a continuation.
+							if (pos < length - 1) {
+								continuation = true;
+							}
+						}
+
+						// Detect possible DOS attacks. Credible strings can never be worse than
+						// the longest (4) escape sequence (3 chars) minus one (the trim).
+						if (num > 4 * 3 - 1) {
+							return '';
+						}
+
+						// Intentionally set a bad hex value
+						high = low = 'e';
+					}
+
+					if (end === length) {
+						return string;
+					}
+
+					return string.substr(0, end);
+				}
+
+				static strip(string) {
+					const length = string.length;
+					const shift = this.hasPercent(string, length);
+
+					// If no % in the last 3 chars, then the string wasn't trimmed.
+					if (shift === 3) {
+						return string;
+					}
+
+					return this._strip(string, length, shift);
+				}
+			}
+
 			return class URLDecode extends Plugin {
 				constructor() {
 					super();
@@ -115,7 +326,9 @@ module.exports = (() => {
 															const newUrl = this.decodeText(elem.props.href);
 															elem.props.title = newUrl;
 															elem.props.children.forEach((element, index, arr) => {
-																arr[index] = arr[index].replace(elem.props.href, newUrl);
+																if (element instanceof String || typeof element === "string") {
+																	arr[index] = arr[index].replace(elem.props.href, newUrl);
+																}
 															});
 															elem.props.href = newUrl;
 														}
@@ -144,9 +357,17 @@ module.exports = (() => {
 						}
 
 						decodeText(text) {
-							if (text)
-								return decodeURIComponent(text.replace(/\+/g, " "));
-							return null;
+							if (text) {
+								text = text.replace(/\+/g, " ");
+								try {
+									return decodeURIComponent(text);
+								} catch {
+									try {
+										return decodeURIComponent(StripInvalidTrailingEncoding.strip(text.slice(0, -3))) + text.slice(-3);
+									} catch {}
+								}
+							}
+							return text;
 						}
 
 						getSettingsPanel() {
